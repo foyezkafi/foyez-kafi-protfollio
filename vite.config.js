5359f942ce7db72d2f5190c226c9f5607b4c4532
Fahim Rahman <fahimrahmandipto@gmail.com>
2026-04-14 01:41:16 +0600

    fix vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/kafi/",
   plugins: [react(), tailwindcss()],
   server: {
     open: true,
